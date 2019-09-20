91ff2780d529642f8ccbc15f8f31857c36e18b49
quangdai <daidoquang@vccorp.vn>
2019-09-20 09:34:02 +0700

    update

--- breadcrumb.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <ul class="main-breadcrumb">
-        <li><a href="">Trang chủ</a></li>
-        <li><a href="">Nam</a></li>
-        <li><a href="" class="current">Giày da Nam</a></li>
-    </ul>
-</template>
--- product_list_pagination.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <ul class="product-list-pagination pagination justify-content-center">
-        <li class="active"><a href="">1</a></li>
-        <li><a href="">2</a></li>
-        <li><a href="">3</a></li>
-        <li><a href="">4</a></li>
-        <li><a href="">5</a></li>
-    </ul>
-</template>
